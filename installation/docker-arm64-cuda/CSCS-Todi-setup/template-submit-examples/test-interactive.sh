3d766f84537e93d339dd8bff2afedf58506ff1fd
Skander Moalla <skander.moalla@epfl.ch>
2024-09-02 15:38:01 +0200

    Change Docker installation to arm64

---
+++ installation/docker-arm64-cuda/CSCS-Todi-setup/template-submit-examples/test-interactive.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Enroot + Pyxis
+
+export PROJECT_ROOT_AT=$SCRATCH/swiss-alignment/dev
+
+srun \
+  -J template-test \
+  --pty \
+  --container-image=$CONTAINER_IMAGES/claire+smoalla+swiss-alignment+arm64-cuda-root-latest.sqsh \
+  --no-container-mount-home \
+  --no-container-remap-root \
+  --no-container-entrypoint \
+  --container-writable \
+  bash
+
+exit 0
