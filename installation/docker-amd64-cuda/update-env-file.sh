c37e01726ad34c2a320a49ddb27c6aa315d725a8
Skander Moalla <skander.moalla@epfl.ch>
2024-09-03 19:39:32 +0200

    amd64 env file

--- installation/docker-amd64-cuda/update-env-file.sh
+++ installation/docker-amd64-cuda/update-env-file.sh
@@ -5,4 +5,6 @@
 # will fix the environment file. (you could also just edit it manually afterwards).
 
 ENV_FILE="${PROJECT_ROOT_AT}"/installation/docker-amd64-cuda/requirements.txt
-pip list --exclude-editable --format freeze > "${ENV_FILE}"
+# Delete bitsandbites which is built from source in the Dockerfile
+# remove the line that starts with bitsandbites
+pip list --exclude-editable --format freeze | sed "/bitsandbytes/d" > "${ENV_FILE}"
