7ac5cf9f578fdd62ce2c0ef4ac9ee9f5095f5098
Skander Moalla <skander.moalla@epfl.ch>
2025-01-13 22:03:32 +0100

    Template

---
+++ src/template_package_name/utils/seeding.py
@@ -0,0 +1,34 @@
+import random
+
+
+def generate_random_seed():
+    """Generate a random seed."""
+    return random.randint(0, 2**32 - 1)
+
+
+# Update this function whenever you have a library that needs to be seeded.
+def seed_everything(config):
+    """Seed all random generators."""
+    random.seed(config.seed)
+
+    ## For numpy:
+    # This is for legacy numpy:
+    # np.random.seed(config.seed)
+    # New code should make a Generator out of the config.seed directly:
+    # https://numpy.org/doc/stable/reference/random/generated/numpy.random.seed.html
+
+    ## For PyTorch:
+    # torch.manual_seed(config.seed)
+
+    # Higher (e.g., on CUDA too) reproducibility with deterministic algorithms:
+    # https://pytorch.org/docs/stable/notes/randomness.html
+
+    # Not supported for all operations though:
+    # https://pytorch.org/docs/stable/generated/torch.use_deterministic_algorithms.html
+    # torch.use_deterministic_algorithms(True)
+
+    #  A lighter version of the above otherwise as not all algorithms have a deterministic implementation
+    # torch.backends.cudnn.deterministic = True
+
+    # torch.backends.cudnn.benchmark = False
+    # os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":4096:8"
