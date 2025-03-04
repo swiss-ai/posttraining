fb3098cdd8f2321e93ca169c93c84409b4b7a3ad
Juan Garcia Giraldo <juan.garciagiraldo@epfl.ch>
2025-03-04 16:06:35 +0100

    cleaning plw

--- reproducibility-scripts/trl-plw/generate_submit.py
+++ reproducibility-scripts/trl-plw/generate_submit.py
@@ -8,7 +8,7 @@ learning_rates = [5e-6, 1e-5, 2e-5]
 ds_accumulation_steps = 8
 
 current_time = datetime.now().strftime("%Y-%m-%d-%H-%M")
-run_name = f"promp_loss_weight-sweep"
+run_name = f"plw-sweep"
 nruns = 0
 for dataset in datasets:
     for model in models:
@@ -20,7 +20,7 @@ for dataset in datasets:
                     f"sbatch --nodes 4 "
                     f"--output=reproducibility-scripts/trl-plw/out-{current_time}/{model_config}/{hp_config}.out "
                     "./installation/docker-arm64-cuda/CSCS-Clariden-setup/shared-submit-scripts/unattended-ds.sh "
-                    f"-m swiss_alignment.trl.train_plw "
+                    f"-m swiss_alignment.trl.plw.train_plw "
                     f"dataset={dataset} "
                     f"model={model} "
                     f"plw_args.prompt_loss_weight={plw} "
