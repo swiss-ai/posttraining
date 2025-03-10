ce3986ef111c2816bcdefb096d7315e4b42904f0
Juan Garcia Giraldo <juan.garciagiraldo@epfl.ch>
2025-03-10 17:25:04 +0100

    modifying compute loss in plw trainer

--- src/swiss_alignment/trl/trainers.py
+++ src/swiss_alignment/trl/trainers.py
@@ -43,6 +43,7 @@ class PLWTrainer(CustomSFTTrainer):
             }
         )
         super().__init__(*args, **kwargs)
+        # self.model_accepts_loss_kwargs = False
         self.plw = torch.tensor(
             prompt_loss_weight, dtype=self.model.dtype, device=self.args.device
         )
@@ -69,6 +70,42 @@ class PLWTrainer(CustomSFTTrainer):
                 self.completion_mask[i, :c_len] = completion_masks[i][:c_len]
         else:
             self.prompt_mask = self.completion_mask = None
+
+    def get_batch_samples(self, epoch_iterator, num_batches):
+        batch_samples = []
+        num_items_in_batch = None
+        for _ in range(num_batches):
+            try:
+                batch_samples += [next(epoch_iterator)]
+            except StopIteration:
+                break
+
+        if (
+            len(batch_samples) > 0
+            and "prompt_mask" in batch_samples[0]
+            and "completion_mask" in batch_samples[0]
+        ):
+            try:
+                num_items_in_batch = sum(
+                    [
+                        (
+                            self.plw * batch["prompt_mask"] + batch["completion_mask"]
+                        ).sum()
+                        for batch in batch_samples
+                    ]
+                )
+            except (TypeError, AttributeError):
+                pass
+
+        if self.args.average_tokens_across_devices and num_items_in_batch is not None:
+            num_items_in_batch = (
+                self.accelerator.gather(num_items_in_batch).sum().item()
+            )
+
+        if torch.is_tensor(num_items_in_batch):
+            num_items_in_batch = num_items_in_batch.item()
+
+        return batch_samples, num_items_in_batch
 
     def compute_loss(
         self, model, inputs, return_outputs=False, num_items_in_batch=None
@@ -97,13 +134,16 @@ class PLWTrainer(CustomSFTTrainer):
         token_losses = loss_fct(shift_logits.transpose(1, 2), shift_labels)
 
         if num_items_in_batch is not None:
-            # TODO: clarify
-            # loss = (token_losses * shift_weights).sum() / shift_weights.sum()
             loss = (token_losses * shift_weights).sum() / num_items_in_batch
-            loss *= self.accelerator.num_processes
         else:
-            # TODO: verify this, because here the "mean" is per mini-batch and does not consider num_items_in_batch
-            loss = (token_losses * shift_weights).mean()
+            loss = (token_losses * shift_weights).sum() / shift_weights.sum()
+
+        if (
+            self.args.average_tokens_across_devices
+            # and (self.model_accepts_loss_kwargs or self.compute_loss_func)
+            and num_items_in_batch is not None
+        ):
+            loss *= self.accelerator.num_processes
 
         return (loss, outputs) if return_outputs else loss
 
@@ -126,15 +166,9 @@ class PLWTrainer(CustomSFTTrainer):
         shift_prompt_mask = self.prompt_mask[:dataset_size, 1:]
         shift_comp_mask = self.completion_mask[:dataset_size, 1:]
 
-        # TODO: clarify this
-        # Compute total/prompt/completion average loss
-        # do we want this? it depends on the compute_loss function when num_items_in_batch is set to None
-        # total_loss = (token_losses * (self.plw.to(dtype=torch.float32).cpu().numpy() * shift_prompt_mask + shift_comp_mask)).mean()
-        prompt_loss = (token_losses * shift_prompt_mask).mean()
-        completion_loss = (token_losses * shift_comp_mask).mean()
-        # or
-        # prompt_loss = (token_losses * shift_prompt_mask).sum() / shift_prompt_mask.sum()
-        # completion_loss = (token_losses * shift_comp_mask).sum() / shift_comp_mask.sum()
+        # Computing prompt/completion loss
+        prompt_loss = (token_losses * shift_prompt_mask).sum() / shift_prompt_mask.sum()
+        completion_loss = (token_losses * shift_comp_mask).sum() / shift_comp_mask.sum()
 
         # Compute total/prompt/completion accuracies
         def compute_accuracy(preds, labels, mask):
