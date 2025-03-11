df83d878ec1877c8a3071a482db439fdc9a90063
Juan Garcia Giraldo <juan.garciagiraldo@epfl.ch>
2025-03-11 14:35:15 +0100

    mounting smoalla storage + improving command scripts

---
+++ installation/docker-arm64-cuda/CSCS-Clariden-setup/shared-submit-scripts/env-vars.sh
@@ -0,0 +1,14 @@
+export PROJECT_NAME=swiss-alignment
+export PACKAGE_NAME=swiss_alignment
+export WANDB_API_KEY_FILE_AT=$HOME/.wandb-api-key
+export HF_TOKEN_AT=$HOME/.hf-token
+export HF_HOME=$SCRATCH/huggingface
+export OPENAI_API_KEY_AT=$HOME/.openai-api-key
+export OPENAI_API_KEY=$(cat $OPENAI_API_KEY_AT)
+
+export WANDB_API_KEY_FILE_AT=$HOME/.wandb-api-key
+if [[ "$@" == *"wandb.anonymize=true"* ]]; then
+    export WANDB_API_KEY_FILE_AT=$HOME/.wandb-api-key-anonymous
+fi
+
+export CUDA_BUFFER_PAGE_IN_THRESHOLD_MS=0.001
