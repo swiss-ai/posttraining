75d173aef204dace08271454099fbb86f1376b48
Juan Garcia Giraldo <juan.garciagiraldo@epfl.ch>
2025-03-11 15:10:52 +0100

    clearning sft script

--- reproducibility-scripts/trl-sft/generate_submit.py
+++ reproducibility-scripts/trl-sft/generate_submit.py
@@ -1,10 +1,17 @@
 from datetime import datetime
 
-models = ["mistralai-mistral-7b-instruct.yaml"]
-datasets = ["magpieair"]
-
-learning_rates = [5e-6, 1e-5, 2e-5]
-ds_accumulation_steps = 8
+models = ["meta-llama-3-1-8b.yaml"]
+datasets = ["tulu-3-sft-mixture-split"]
+num_epochs = 2
+learning_rates = [5e-6]
+batch_size = 128
+num_nodes = 2
+num_proc_per_node = 4
+proc_train_batch_size = 2
+accumulation_steps = batch_size // (
+    num_nodes * num_proc_per_node * proc_train_batch_size
+)
+proc_eval_batch_size = 2
 
 current_time = datetime.now().strftime("%Y-%m-%d-%H-%M")
 run_name = f"example-hp-sweep"
@@ -15,13 +22,19 @@ for dataset in datasets:
             model_config = f"{model}-{dataset}"
             hp_config = f"lr-{lr}"
             command = (
-                f"sbatch --nodes 4 "
+                f"sbatch "
+                f"--nodes {num_nodes} "
                 f"--output=reproducibility-scripts/trl-sft/out-{current_time}/{model_config}/{hp_config}.out "
                 "./installation/docker-arm64-cuda/CSCS-Clariden-setup/shared-submit-scripts/unattended-ds.sh "
                 f"-m swiss_alignment.trl.train_sft "
                 f"dataset={dataset} "
                 f"model={model} "
-                f"training_args.gradient_accumulation_steps={ds_accumulation_steps} "
+                f"training_args.num_train_epochs={num_epochs} "
+                f"training_args.gradient_accumulation_steps={accumulation_steps} "
+                f"training_args.per_device_train_batch_size={proc_train_batch_size} "
+                f"training_args.per_device_eval_batch_size={proc_eval_batch_size} "
+                f"training_args.learning_rate={lr} "
+                f"training_args.lr_scheduler_type={lr} "
                 f"training_args.learning_rate={lr} "
                 "outputs_subdir=shared "
                 f"job_subdir={run_name}/{model_config} "
