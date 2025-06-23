bef2325a83dfa36f0cd86ce7b69cd9f6c5783dd5
Skander Moalla <37197319+skandermoalla@users.noreply.github.com>
2025-06-23 11:48:33 +0200

    Update remote-development.sh

--- installation/docker-arm64-cuda/CSCS-Clariden-setup/shared-submit-scripts/remote-development.sh
+++ installation/docker-arm64-cuda/CSCS-Clariden-setup/shared-submit-scripts/remote-development.sh
@@ -26,7 +26,7 @@ export OMP_NUM_THREADS=1
 export TOKENIZERS_PARALLELISM=false
 
 srun \
-  --container-image=$CONTAINER_IMAGES/$(id -gn)+$(id -un)+swiss-alignment+arm64-cuda-root-latest.sqsh \
+  --container-image=$CONTAINER_IMAGES/infra01+$(id -un)+swiss-alignment+arm64-cuda-root-latest.sqsh \
   --environment="${PROJECT_ROOT_AT}/installation/docker-arm64-cuda/CSCS-Clariden-setup/shared-submit-scripts/edf.toml" \
   --container-mounts=\
 $PROJECT_ROOT_AT,\
