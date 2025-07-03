5b0060ade65bec5919d2d010074fdbcbb6b7258e
Juan Garcia Giraldo <juan.garciagiraldo@epfl.ch>
2025-07-03 17:12:35 +0200

    adding minor updates to scripts

---
+++ reproducibility-scripts/trl-plw/apertus/generate_submit_apertus_8b.py
@@ -0,0 +1,95 @@
+from datetime import datetime
+
+models = ["apertus3-8b"]
+datasets = ["swissai-tulu-3-sft-0225"]
+# datasets = ["if-english", "if-prompt-translated", "if-multilingual"]
+
+num_epochs = 2
+batch_size = 128
+max_seq_length = 4096
+num_nodes = 8
+num_proc_per_node = 4
+proc_train_batch_size = 1
+accumulation_steps = batch_size // (
+    num_nodes * num_proc_per_node * proc_train_batch_size
+)
+proc_eval_batch_size = 2
+
+learning_rates = [5e-6]  # 8b: 5e-6; 70b: 2e-6
+lr_scheduler_type = "linear"
+lr_warmup_ratio = 0.03
+
+trainer = "plw"  # can only take values: sft, plw, ln-plw, irl
+# prompt_loss_weight = [0.0, 0.01, 0.1, 0.5, 1.0]
+prompt_loss_weight = [
+    0.0,
+]  # where sft -> plw=1.0
+
+logging_steps = 1
+# eval_steps = 1000
+save_steps = 1000
+
+current_time = datetime.now().strftime("%Y-%m-%d-%H-%M")
+run_name = f"apertus3-8b-sweep"
+nruns = 0
+for dataset in datasets:
+    for model in models:
+        for iter in [
+            "Apertus8B-tokens2T-it478000",
+            "Apertus8B-tokens3T-it716000",
+            "Apertus8B-tokens4T-it954000",
+            "Apertus8B-tokens5T-it1194000",
+            "Apertus8B-tokens6T-it1432000",
+            "Apertus8B-tokens7T-it1670000",
+            "Apertus8B-tokens7.04T-it1678000",
+        ]:
+            for lr in learning_rates:
+                for plw in prompt_loss_weight:
+                    model_config = f"{iter}-{dataset}"
+                    hp_config = f"{trainer}-{plw}-lr-{lr}"
+                    command = (
+                        f"sbatch "
+                        f"--nodes {num_nodes} "
+                        f"--output=reproducibility-scripts/trl-plw/out-{current_time}/{model_config}/{hp_config}.out "
+                        "./installation/docker-arm64-cuda/CSCS-Clariden-setup/shared-submit-scripts/unattended-ds.sh "
+                        f"-m swiss_alignment.trl.plw.train_plw "
+                        f"dataset={dataset} "
+                        # f"dataset_args.debug_oom=true "
+                        # f"dataset_args.debug_subsample.train=50_000 "
+                        # f"dataset_args.debug_subsample.eval=100 "
+                        f"model={model}.yaml "
+                        f"model_args.model_name_or_path=/capstor/store/cscs/swissai/infra01/pretrain-checkpoints/apertus/{iter} "
+                        f"tokenizer_args.tokenizer_name_or_path=/capstor/store/cscs/swissai/infra01/pretrain-checkpoints/apertus/{iter} "
+                        f"trainer={trainer} "
+                        f"plw_args.prompt_loss_weight={plw} "
+                        f"training_args.max_seq_length={max_seq_length} "
+                        # f"training_args.max_grad_norm=1 "
+                        f"training_args.num_train_epochs={num_epochs} "
+                        f"training_args.gradient_accumulation_steps={accumulation_steps} "
+                        f"training_args.per_device_train_batch_size={proc_train_batch_size} "
+                        f"training_args.per_device_eval_batch_size={proc_eval_batch_size} "
+                        f"training_args.learning_rate={lr} "
+                        f"training_args.lr_scheduler_type={lr_scheduler_type} "
+                        f"training_args.warmup_ratio={lr_warmup_ratio} "
+                        f"training_args.logging_steps={logging_steps} "
+                        f"training_args.eval_strategy=no "
+                        # f"training_args.eval_steps={eval_steps} "
+                        f"training_args.eval_on_start=false "
+                        # f"training_args.save_strategy=epoch "
+                        f"training_args.save_strategy=steps "
+                        f"training_args.save_steps={save_steps} "
+                        f"tokenizer_args.chat_template_name=tulu "
+                        "outputs_subdir=shared "
+                        f"job_subdir={run_name}/{model_config} "
+                        f"wandb.run_name={model_config} "
+                        f"wandb.tags=[prod,{trainer}] "
+                        "resuming.resume=True "
+                    )
+                    print(command)
+                    nruns += 1
+
+print(nruns)
+
+
+# sbatch --dependency=afterany:539566 --node 32 --output=reproducibility-scripts/trl-plw/out-2025-07-02-17-04/Apertus70B-tokens7T-it679000-swissai-tulu-3-sft-0225/plw-0.0-lr-2e-06.out ./installation/docker-arm64-cuda/CSCS-Clariden-setup/shared-submit-scripts/unattended-ds-zero3.sh -m swiss_alignment.trl.plw.train_plw dataset=swissai-tulu-3-sft-0225 model=apertus3-70b.yaml model_args.model_name_or_path=/capstor/store/cscs/swissai/infra01/pretrain-checkpoints/apertus/Apertus70B-tokens7T-it679000tokenizer_args.tokenizer_name_or_path=/capstor/store/cscs/swissai/infra01/pretrain-checkpoints/apertus/Apertus70B-tokens7T-it679000 trainer=plw plw_args.prompt_loss_weight=0.0 training_args.max_seq_length=4096 training_args.num_train_epochs=2 training_args.gradient_accumulation_steps=1 training_args.per_device_train_batch_size=1 training_args.per_device_eval_batch_size=2 training_args.learning_rate=2e-06 training_args.lr_scheduler_type=linear training_args.warmup_ratio=0.03 training_args.logging_steps=1 training_args.eval_strategy=no training_args.eval_on_start=false training_args.save_strategy=steps training_args.save_steps=1000 tokenizer_args.chat_template_name=tulu outputs_subdir=shared job_subdir=apertus3-70b-sweep/Apertus70B-tokens7T-it679000-swissai-tulu-3-sft-0225 wandb.run_name=Apertus70B-tokens7T-it679000-swissai-tulu-3-sft-0225 wandb.tags=[prod,plw] resuming.resume=True
+# sbatch --dependency=afterany:531442 --nodes 8 --output=reproducibility-scripts/trl-plw/out-2025-06-26-08-38/apertus3-8b_iter_1678000-swissai-tulu-3-sft-0225/plw-0.0-lr-5e-06.out ./installation/docker-arm64-cuda/CSCS-Clariden-setup/shared-submit-scripts/unattended-ds.sh -m swiss_alignment.trl.plw.train_plw dataset=swissai-tulu-3-sft-0225 model=apertus3-8b.yaml trainer=plw plw_args.prompt_loss_weight=0.0 training_args.max_seq_length=4096 training_args.num_train_epochs=2 training_args.gradient_accumulation_steps=4 training_args.per_device_train_batch_size=1 training_args.per_device_eval_batch_size=2 training_args.learning_rate=5e-06 training_args.lr_scheduler_type=linear training_args.warmup_ratio=0.03 training_args.logging_steps=1 training_args.eval_strategy=no training_args.eval_on_start=false training_args.save_strategy=steps training_args.save_steps=1000 tokenizer_args.chat_template_name=tulu outputs_subdir=shared job_subdir=apertus3-8b-sweep/apertus3-8b_iter_1678000-swissai-tulu-3-sft-0225 wandb.run_name=apertus3-8b_iter_1678000-swissai-tulu-3-sft-0225 wandb.tags=[prod,plw] resuming.resume=True
---
+++ reproducibility-scripts/trl-plw/apertus/generate_submit_hyperparam_serach.py
@@ -0,0 +1,103 @@
+from datetime import datetime
+from itertools import product
+from pathlib import Path
+
+current_time = datetime.now().strftime("%Y-%m-%d-%H-%M")
+stdout_root = (
+    Path(__file__).parent.resolve().relative_to(Path.cwd()) / f"out-{current_time}"
+)
+
+
+models = ["apertus3-70b"]
+datasets = ["swissai-tulu-3-sft-0225"]
+
+# Hyperparameters
+num_epochs = 2  # we save intermediate checkpoints
+max_seq_length = 4096
+learning_rates = [5e-7, 1e-6, 2e-6, 5e-6]
+batch_size = [(128, 32), (512, 64), (1024, 128)]  # bs, num_nodes
+grad_clipping = [1, 5, 1_000]
+trainers = [  # Trainers available: sft, plw, ln-plw, irl
+    ("ln-plw", 0.0),
+    ("ln-plw", 0.01),
+    ("ln-plw", 0.1),
+    ("irl", 0.0),
+    ("irl", 0.01),
+    ("irl", 0.1),
+]
+chat_templates = ["simple_concat_with_space", "simple_chat", "tulu"]
+
+lr_scheduler_type = "linear"  # TODO
+lr_warmup_ratio = 0.03  # TODO
+
+
+# TODO
+num_proc_per_node = 4
+proc_train_batch_size = 1
+proc_eval_batch_size = 2
+
+commands = []
+run_name = f"apertus3-70b-sweep"
+for (
+    dataset,
+    model,
+    lr,
+    (bs, num_nodes),
+    grad_clip,
+    (trainer, plw_weight),
+    chat_template,
+) in product(
+    datasets,
+    models,
+    learning_rates,
+    batch_size,
+    grad_clipping,
+    trainers,
+    chat_templates,
+):
+    model_config = f"{model}-{dataset}"
+    hp_config = f"lr={lr}-bs={bs}-grad_clip={grad_clip}-trainer={trainer}-plw_weight={plw_weight}-chat_template={chat_template}"
+    command = (
+        f"sbatch "
+        f"--nodes {num_nodes} "
+        f"--output={stdout_root}/{hp_config}.out "
+        f"--error={stdout_root}/{hp_config}.err "
+        "./installation/docker-arm64-cuda/CSCS-Clariden-setup/shared-submit-scripts/unattended-ds-zero3.sh "
+        f"-m swiss_alignment.trl.plw.train_plw "
+        f"dataset={dataset} "
+        f"model={model}.yaml "
+        f"tokenizer_args.chat_template_name={chat_template} "
+        f"trainer={trainer} "
+        f"plw_args.prompt_loss_weight={plw_weight} "
+        f"training_args.max_seq_length={max_seq_length} "
+        f"training_args.max_grad_norm={grad_clip} "
+        f"training_args.num_train_epochs={num_epochs} "
+        f"training_args.gradient_accumulation_steps={bs // (num_nodes * num_proc_per_node * proc_train_batch_size)} "
+        f"training_args.per_device_train_batch_size={proc_train_batch_size} "
+        f"training_args.per_device_eval_batch_size={proc_eval_batch_size} "
+        f"training_args.learning_rate={lr} "
+        f"training_args.lr_scheduler_type={lr_scheduler_type} "
+        f"training_args.warmup_ratio={lr_warmup_ratio} "
+        f"training_args.logging_steps=1 "
+        f"training_args.eval_strategy=no "
+        f"training_args.eval_on_start=false "
+        f"training_args.save_strategy=steps "
+        f"training_args.save_steps=1000 "
+        "outputs_subdir=shared "
+        f"job_subdir={run_name}/{model_config}/{hp_config} "
+        f"wandb.run_name={model_config} "
+        f"wandb.tags=[prod,{trainer}] "
+        "resuming.resume=True "
+    )
+    print(command)
+    commands.append(command)
+
+# Write th submit commands to a new directory where this batch of experiments will be managed)
+# Path from the project root
+submit_dir = Path.cwd() / str(stdout_root)
+submit_dir.mkdir(parents=True, exist_ok=True)
+submit_file = submit_dir / "submit_hyperparam_search.sh"
+print(f"Writing {len(commands)} commands to {submit_file}")
+with open(submit_file, "w") as f:
+    for command in commands:
+        f.write(command + "\n")
