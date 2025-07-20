7774be5390f0f0e73eb3950a2cd2a60da910fe6d
Skander Moalla <skander.moalla@epfl.ch>
2025-07-20 19:15:41 +0200

    fix artifact subdir

--- installation/docker-arm64-cuda/build-vllm/build.sh
+++ installation/docker-arm64-cuda/build-vllm/build.sh
@@ -1,2 +1,2 @@
-podman build --tag vllm:v0.9.0.1-apertus-vllm Dockerfile-vllm-apertus
-podman build --tag vllm:v0.9.0.1-base-vllm Dockerfile-vllm-base
+podman build --tag vllm:v0.9.0.1-apertus-vllm --file Dockerfile-apertus-vllm .
+podman build --tag vllm:v0.9.0.1-base-vllm --file Dockerfile-base-vllm .
