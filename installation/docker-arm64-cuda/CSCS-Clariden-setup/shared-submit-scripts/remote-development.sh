caa42f7e6e4a264707c07d8f6aa2d82119bdec34
Mete Ismayilzada <mismayilza@gmail.com>
2025-08-18 22:44:44 +0200

    update submit scripts to main

--- installation/docker-arm64-cuda/CSCS-Clariden-setup/shared-submit-scripts/remote-development.sh
+++ installation/docker-arm64-cuda/CSCS-Clariden-setup/shared-submit-scripts/remote-development.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 #SBATCH -J swiss-alignment-dev
-#SBATCH -t 1:30:00
-#SBATCH -p debug
+#SBATCH -t 12:00:00
 #SBATCH -A a-infra01-1
 #SBATCH --output=sremote-development.out
 #SBATCH --nodes 1
