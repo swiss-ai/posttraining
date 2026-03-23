b19a31821a022d02c685b77bae6e2a8df2b1d7de
Simon Matrenok <semen.matrenok@epfl.ch>
2026-03-23 15:16:52 +0100

    patch paths in artifacts/setup-symlinks.sh

--- artifacts/setup-symlinks.sh
+++ artifacts/setup-symlinks.sh
@@ -5,9 +5,9 @@ if [ -z "$INSTANCE" ]; then
   exit 1
 fi
 
-rm -f /users/$(id -un)/projects/swiss-alignment/$INSTANCE/artifacts/private
-rm -f /users/$(id -un)/projects/swiss-alignment/$INSTANCE/artifacts/shared
-rm -f /users/$(id -un)/projects/swiss-alignment/$INSTANCE/artifacts/store
-ln -s /iopsstor/scratch/cscs/$(id -un)/projects/swiss-alignment/artifacts/private /users/$(id -un)/projects/swiss-alignment/$INSTANCE/artifacts/private
-ln -s /iopsstor/scratch/cscs/smoalla/projects/swiss-alignment/artifacts/shared /users/$(id -un)/projects/swiss-alignment/$INSTANCE/artifacts/shared
-ln -s /capstor/store/cscs/swissai/infra01/swiss-alignment/artifacts /users/$(id -un)/projects/swiss-alignment/$INSTANCE/artifacts/store
+rm -f /users/$(id -un)/projects/posttraining/$INSTANCE/artifacts/private
+rm -f /users/$(id -un)/projects/posttraining/$INSTANCE/artifacts/shared
+rm -f /users/$(id -un)/projects/posttraining/$INSTANCE/artifacts/store
+ln -s /iopsstor/scratch/cscs/$(id -un)/projects/swiss-alignment/artifacts/private /users/$(id -un)/projects/posttraining/$INSTANCE/artifacts/private
+ln -s /iopsstor/scratch/cscs/smoalla/projects/swiss-alignment/artifacts/shared /users/$(id -un)/projects/posttraining/$INSTANCE/artifacts/shared
+ln -s /capstor/store/cscs/swissai/infra01/swiss-alignment/artifacts /users/$(id -un)/projects/posttraining/$INSTANCE/artifacts/store
